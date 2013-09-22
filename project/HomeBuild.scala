959de965907b2b6c72aee472c04921d6b1084a9a
malliina <malliina123@gmail.com>
2013-09-22 10:44:40 +0200

    2.2.0

--- project/HomeBuild.scala
+++ project/HomeBuild.scala
@@ -5,20 +5,15 @@ import Dependencies._
 import com.mle.sbt.cloud._
 
 object HomeBuild extends Build {
-  lazy val homepage = play.Project("homepage",
-    path = file("."),
-    applicationVersion = "0.1",
-    dependencies = Seq(scalaTest),
-    settings = commonSettings
-  )
-  // Hack for play compat
-  override def settings = super.settings ++ com.typesafe.sbtidea.SbtIdeaPlugin.ideaSettings
+  lazy val homepage = Project("homepage", file("."), settings = commonSettings)
 
   val commonSettings = Defaults.defaultSettings ++ Seq(
-    scalaVersion := "2.10.0",
+    scalaVersion := "2.10.2",
+    version := "1.1.0",
+    libraryDependencies += scalaTest,
     retrieveManaged := false,
     sbt.Keys.fork in Test := true,
     resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
     HerokuKeys.heroku := Paths.get( """C:\Program Files (x86)\Heroku\bin\heroku.bat""")
-  ) ++ HerokuPlugin.settings
+  ) ++ HerokuPlugin.settings ++ play.Project.playScalaSettings
 }
--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -5,18 +5,21 @@ import sbt.Keys._
  *
  * @author mle
  */
-object WebBuildBuild extends Build {
+object BuildBuild extends Build {
   // "build.sbt" goes here
   override lazy val settings = super.settings ++ Seq(
-    // play doesn't like 2.10 here
-    scalaVersion := "2.9.2",
-    resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-    resolvers += "Typesafe ivy releases" at "http://repo.typesafe.com/typesafe/ivy-releases/",
-    resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-    scalacOptions ++= Seq("-unchecked", "-deprecation"),
-    addSbtPlugin("play" % "sbt-plugin" % "2.1.1"),
-    addSbtPlugin("com.github.malliina" %% "sbt-paas-deployer" % "0.102")
-  )
+    scalaVersion := "2.10.2",
+    resolvers ++= Seq(
+      "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+      "Typesafe ivy releases" at "http://repo.typesafe.com/typesafe/ivy-releases/",
+      "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"),
+    scalacOptions ++= Seq("-unchecked", "-deprecation")
+  ) ++ sbtPlugins
+
+  def sbtPlugins = Seq(
+    "com.typesafe.play" % "sbt-plugin" % "2.2.0",
+    "com.github.malliina" %% "sbt-paas-deployer" % "1.0.0"
+  ) map addSbtPlugin
 
   override lazy val projects = Seq(root)
   lazy val root = Project("plugins", file("."))
