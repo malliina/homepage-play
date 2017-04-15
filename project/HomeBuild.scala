135a13473df2185c0cd09dfc88d2af90a37d0254
Michael Skogberg <michael.skogberg@outlook.com>
2017-04-15 14:41:40 +0300

    Up build deps

--- project/HomeBuild.scala
+++ project/HomeBuild.scala
@@ -1,33 +1,24 @@
 import com.malliina.sbt.unix.LinuxKeys._
 import com.malliina.sbt.unix.LinuxPlugin
 import com.malliina.sbtplay.PlayProject
-import com.typesafe.sbt.SbtNativePackager.{Debian, Linux, Universal}
-import com.typesafe.sbt.packager.Keys.{maintainer, serverLoading}
-import com.typesafe.sbt.packager.archetypes.{JavaServerAppPackaging, ServerLoader}
+import com.typesafe.sbt.SbtNativePackager.Linux
+import com.typesafe.sbt.packager.Keys.maintainer
 import sbt.Keys._
 
 object HomeBuild {
   lazy val homepage = PlayProject.default("homepage")
-    .enablePlugins(JavaServerAppPackaging)
     .settings(commonSettings: _*)
 
   val commonSettings = linuxSettings ++ Seq(
     scalaVersion := "2.11.8",
-    version := "1.3.2"
+    version := "1.3.4"
   )
 
   def linuxSettings = {
     LinuxPlugin.playSettings ++ Seq(
       httpPort in Linux := Option("8461"),
       httpsPort in Linux := Option("disabled"),
-      maintainer := "Michael Skogberg <malliina123@gmail.com>",
-      javaOptions in Universal ++= {
-        Seq(
-          "-Dfile.encoding=UTF-8",
-          "-Dsun.jnu.encoding=UTF-8"
-        )
-      },
-      serverLoading in Debian := ServerLoader.Systemd
+      maintainer := "Michael Skogberg <malliina123@gmail.com>"
     )
   }
 }
--- project/project/BuildBuild.scala
+++ project/project/BuildBuild.scala
@@ -6,16 +6,13 @@ object BuildBuild {
   lazy val settings = Seq(
     scalaVersion := "2.10.6",
     resolvers ++= Seq(
-      "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-      "Typesafe ivy releases" at "http://repo.typesafe.com/typesafe/ivy-releases/",
-      "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
-      Resolver.url("malliina bintray sbt", url("https://dl.bintray.com/malliina/sbt-plugins/"))(Resolver.ivyStylePatterns)
+      Resolver.url("malliina bintray sbt", url("https://dl.bintray.com/malliina/sbt-plugins/"))(Resolver.ivyStylePatterns),
+      Resolver.bintrayRepo("malliina", "maven")
     ),
     scalacOptions ++= Seq("-unchecked", "-deprecation")
   ) ++ sbtPlugins
 
   def sbtPlugins = Seq(
-    "com.malliina" %% "sbt-play" % "0.9.1",
-    "com.malliina" %% "sbt-packager" % "2.2.0"
+    "com.malliina" %% "sbt-play" % "0.9.5"
   ) map addSbtPlugin
 }
